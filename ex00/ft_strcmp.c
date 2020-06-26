9117abb36b742a9cf54f938822f8155a50236997
sbaloyi <66995239+sbaloyi@users.noreply.github.com>
2020-06-26 14:25:09 +0200

    Updated function with pointers in place for arrays

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -1,7 +1,9 @@
-int	ft_strcmp(char *s1, char *s2)
+int ft_strcmp(const char *a, const char *b)
 {
-	int counter = 0;
-	while (s1[counter] != '\0' && s2[counter] != '\0' && s1[counter] == s2[counter]) 
-		counter++;
-	return (s1[counter] - s2[counter]);
+    while (*a && *a == *b)
+    {
+        a++;
+        b++;
+    }
+    return *a - *b;
 }
