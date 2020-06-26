59ecfb0f0a6d0bb79b44e8bd8b25a11f391808d1
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-26 14:19:07 +0200

    Inital ex03 commit

---
+++ ex03/ft_rev_params.c
@@ -0,0 +1,22 @@
+void ft_putchar(char c) {
+	write(1, &c, 1);
+}
+
+int main(int argc, char *argv[])
+{
+	int counter = 0;
+	int counter1 = argc - 1;
+
+	while(counter1 > 0)
+	{
+		while(argv[counter1][counter])
+		{
+			ft_putchar(argv[counter1][counter]);
+			counter++;
+		}
+		counter1--;
+		counter = 0;
+		ft_putchar('\n');
+	}
+	return (0);
+}
