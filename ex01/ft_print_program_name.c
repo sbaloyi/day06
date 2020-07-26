0beacc435e4c1b30654da81f9cc16bd22f7bc9b8
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-07-26 16:20:11 +0200

    Updated ft_print_program_name

--- ex01/ft_print_program_name.c
+++ ex01/ft_print_program_name.c
@@ -5,12 +5,12 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int counter = 0;
 	while(argv[0][counter] != '\0')
 	{
-		ft_putchar(av[0][counter]);
+		ft_putchar(argv[0][counter]);
 		counter++;
 	}
 	ft_putchar('\n');
